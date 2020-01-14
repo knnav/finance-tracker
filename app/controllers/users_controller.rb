8a67a07669a93a44bef20517d5929346291c6fd0
Kurt <kurt.norenbergs@gmail.com>
2020-01-14 17:16:50 -0300

    add friends actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,37 @@ class UsersController < ApplicationController
   end
 
   def my_friends
+    @friendships = current_user.friends
+  end
 
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = 'You just made an empty query.'
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_users(@users)
+      flash.now[:danger] = "No users match the search criteria" unless @users
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+      
+    if current_user.save
+      flash[:success] = "Friend added!"
+    else
+      flash[:danger] = "There was something wrong with the friend requests"
+    end
+
+    redirect_to my_friends_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
   end
 end
